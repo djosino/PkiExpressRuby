2b05ccb1752257da5405453948f951d3e2ab6ca6
EmilyS <emily.souza.rs@gmail.com>
2021-01-07 18:04:50 -0300

    Fixed Digest Algorithm

--- lib/pki_express/digest_algorithm.rb
+++ lib/pki_express/digest_algorithm.rb
@@ -9,22 +9,77 @@ module PkiExpress
 
   class DigestAlgorithm
     attr_accessor :byte_length, :api_model, :xml_uri, :oid, :name
+    def initialize(name, oid, byte_length, api_model, xml_uri)
+      @name = name
+      @oid = oid
+      @byte_length = byte_length
+      @api_model = api_model
+      @xml_uri = xml_uri
+    end
 
-    def self.populate_algorithms
-      unless @algorithms
-        @algorithms = []
-        @algorithms << MD5DigestAlgorithm.new
-        @algorithms << SHA1DigestAlgorithm.new
-        @algorithms << SHA256DigestAlgorithm.new
-        @algorithms << SHA384DigestAlgorithm.new
-        @algorithms << SHA512DigestAlgorithm.new
+    def md5
+      unless @md5
+        @md5 = new(
+          DigestAlgorithms.MD5,
+          Oids::MD5,
+          16,
+          'md5',
+          'http://www.w3.org/2001/04/xmldsig-more#md5')
       end
     end
-    private_class_method :populate_algorithms
+
+    def sha1
+      unless @sha1
+        @sha1 = new(
+          DigestAlgorithms.SHA1,
+          Oids::SHA1,
+          20,
+          'sha1',
+          'http://www.w3.org/2000/09/xmldsig#sha1')
+      end
+    end
+
+    def sha256
+      unless @sha256
+        @sha256 = new(
+          DigestAlgorithms.SHA256,
+          Oids::SHA256,
+          32,
+          'sha256',
+          'http://www.w3.org/2001/04/xmlenc#sha256')
+      end
+    end
+
+    def sha384
+      unless @sha384
+        @sha384 = new(
+          DigestAlgorithms.SHA384,
+          Oids::SHA384,
+          48,
+          'sha384',
+          'http://www.w3.org/2001/04/xmldsig-more#sha384')
+      end
+    end
+
+    def sha512
+      unless @sha512
+        @sha512 = new(
+          DigestAlgorithms.SHA512,
+          Oids::SHA512,
+          64,
+          'sha512',
+          'http://www.w3.org/2001/04/xmlenc#sha512')
+      end
+    end
+
+    def self.get_algorithms
+      return [md5, sha1, sha256, sha384, sha512]
+    end
+    private_class_method :get_algorithms, :new
 
     class << DigestAlgorithm
       def get_instance_by_name(name)
-        populate_algorithms
+        get_algorithms
         unless @algorithms.select{|v| v.name == name}.empty?
           return @algorithms.select{|v| v.name == name}.first
         end
@@ -32,7 +87,7 @@ module PkiExpress
       end
 
       def get_instance_by_oid(oid)
-        populate_algorithms
+        get_algorithms
         unless @algorithms.select{|v| v.oid == oid}.empty?
           return @algorithms.select{|v| v.oid == oid}.first
         end
@@ -40,7 +95,7 @@ module PkiExpress
       end
 
       def get_instance_by_xml_uri(xml_uri)
-        populate_algorithms
+        get_algorithms
         unless @algorithms.select{|v| v.xml_uri == xml_uri}.empty?
           return @algorithms.select{|v| v.xml_uri == xml_uri}.first
         end
@@ -48,62 +103,12 @@ module PkiExpress
       end
 
       def get_instance_by_api_model(api_model)
-        populate_algorithms
+        get_algorithms
         unless @algorithms.select{|v| v.api_model.downcase == api_model.downcase}.empty?
           return @algorithms.select{|v| v.api_model.downcase == api_model.downcase}.first
         end
-        raise 'Unrecognized digest algorithm XML URI: ' + api_model
+        raise 'Unrecognized digest algorithm: ' + api_model
       end
-    end
-  end
-
-  class MD5DigestAlgorithm < DigestAlgorithm
-    def initialize
-      @name = DigestAlgorithms.MD5
-      @oid = Oids.MD5
-      @byte_length = 16
-      @api_model = 'md5'
-      @xml_uri = 'http://www.w3.org/2001/04/xmldsig-more#md5'
-    end
-  end
-
-  class SHA1DigestAlgorithm < DigestAlgorithm
-    def initialize
-      @name = DigestAlgorithms.SHA1
-      @oid = Oids.SHA1
-      @byte_length = 20
-      @api_model = 'sha1'
-      @xml_uri = 'http://www.w3.org/2000/09/xmldsig#sha1'
-    end
-  end
-
-  class SHA256DigestAlgorithm < DigestAlgorithm
-    def initialize
-      @name = DigestAlgorithms.SHA256
-      @oid = Oids.SHA256
-      @byte_length = 32
-      @api_model = 'sha256'
-      @xml_uri = 'http://www.w3.org/2001/04/xmlenc#sha256'
-    end
-  end
-
-  class SHA384DigestAlgorithm < DigestAlgorithm
-    def initialize
-      @name = DigestAlgorithms.SHA384
-      @oid = Oids.SHA384
-      @byte_length = 48
-      @api_model = 'sha384'
-      @xml_uri = 'http://www.w3.org/2001/04/xmldsig-more#sha384'
-    end
-  end
-
-  class SHA512DigestAlgorithm < DigestAlgorithm
-    def initialize
-      @name = DigestAlgorithms.SHA512
-      @oid = Oids.SHA512
-      @byte_length = 64
-      @api_model = 'sha512'
-      @xml_uri = 'http://www.w3.org/2001/04/xmlenc#sha512'
     end
   end
 end
